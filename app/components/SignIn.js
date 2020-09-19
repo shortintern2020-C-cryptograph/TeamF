b5ae9e0ec15b8c186c81bbad9ab86b2ad9db5ac4
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2020-09-19 11:09:45 +0900

    modal の transition

--- app/components/SignIn.js
+++ app/components/SignIn.js
@@ -59,13 +59,14 @@ const SignIn = () => {
       boxShadow: '0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2)'
     },
     overlay: {
-      backgroundColor: 'rgba(255, 255, 255, 0.75)'
+      // 良い感じの色がわかりません
+      backgroundColor: 'rgba(50, 50, 50, 0.6)'
     }
   }
 
   return (
     <Modal
-      // closeTimeoutMS={500}
+      closeTimeoutMS={100}
       isOpen={signInModalOpen}
       onRequestClose={closeModal}
       onAfterOpen={afterOpenModal}
