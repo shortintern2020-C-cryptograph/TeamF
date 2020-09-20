2ac586748bbe4ccfc8989a84a070bbef94d5d561
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2020-09-20 23:37:29 +0900

    未ログイン時は投稿できないように

--- app/components/Fab.js
+++ app/components/Fab.js
@@ -1,15 +1,25 @@
 import { useContext } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
 import { MainContext } from '../contexts/MainContext'
 import styles from '../styles/Fab.module.scss'
 
 const Fab = () => {
   const { fabMode, nextFabMode } = useContext(MainContext)
+  const { user, setSignInModalOpen } = useContext(AuthContext)
   const blackStyle = 'linear-gradient(120deg, #222, #555)'
   const redStyle = 'linear-gradient(120deg, #F05353, #E9468A)'
   const isBlackStyle = fabMode === 'home' || fabMode === 'detail'
 
+  const handleChangeMode = () => {
+    if (!user) {
+      setSignInModalOpen(true)
+      return
+    }
+    nextFabMode(fabMode)
+  }
+
   return (
-    <div className={styles.container} onClick={() => nextFabMode(fabMode)}>
+    <div className={styles.container} onClick={handleChangeMode}>
       <span
         className={styles.icon}
         style={{
