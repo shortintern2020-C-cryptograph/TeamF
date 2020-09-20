7fdff5ff1e5ff95e6caad0f9ce62b9265665aac4
furiko <furiko522code@gmail.com>
2020-09-20 17:37:00 +0900

    panicが起きた時にもログを吐けるように変更。入力データのValidationでエラー起きた際には400で統一して返すように変更。

--- server/gen/restapi/configure_secen_pick_server.go
+++ server/gen/restapi/configure_secen_pick_server.go
@@ -61,6 +61,8 @@ func configureAPI(api *scenepicks.SecenPickServerAPI) http.Handler {
 
 	api.ServerShutdown = func() {}
 
+	errors.DefaultHTTPCode = http.StatusBadRequest
+
 	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
 }
 
@@ -79,7 +81,7 @@ func configureServer(s *http.Server, scheme, addr string) {
 // The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
 // The middleware executes after routing but before authentication, binding and validation
 func setupMiddlewares(handler http.Handler) http.Handler {
-	return mymiddleware.AccessLog(handler)
+	return mymiddleware.Recovery(mymiddleware.AccessLog(handler))
 }
 
 // The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
