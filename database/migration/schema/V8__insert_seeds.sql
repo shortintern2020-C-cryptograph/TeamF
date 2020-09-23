5fac1b3bd8310d3819d7e98c2148fa840b6623d6
e185764 <tus.0812.kai@gmail.com>
2020-09-24 00:17:10 +0900

    動画リンク表示修正

--- database/migration/schema/V8__insert_seeds.sql
+++ database/migration/schema/V8__insert_seeds.sql
@@ -13,14 +13,15 @@ VALUES
 INSERT INTO
   dialog(id, content, title, author, source, link, style, user_id)
 VALUES
-  (1, "バルス", "天空の城ラピュタ", "宮崎駿", "anime", "https://example.com", "normal", 1),
-  (2, "ブンブンハローYoutube", "モンスト100連ガチャやってみた", "HIKAKIN", "youtube", "https://example.com", "normal", 1),
-  (3, "人間はなぜこうまでして自分の似姿をつくりたがるのかしらね", "攻殻機動隊", "草薙素子", "anime", "https://example.com", "normal", 2),
-  (4, "真実はいつも一つ！", "名探偵コナン", "青山剛昌", "anime", "https://example.com", "normal", 1),
-  (5, "これはお前が始めた物語だろ", "進撃の巨人", "諫山創", "anime", "https://example.com", "normal", 3),
-  (6, "俺の大将は王様だが、お前のは玉だ！", "将棋", "東海オンエア", "youtube", "https://example.com", "normal", 4),
-  (7, "グーみたいな奴がいて、チョキみたいな奴もいて、パーみたいな奴もいる。誰が一番強いか答えを知ってる奴はいるか？", "宇宙兄弟", "小山宙哉", "manga", "https://example.com", "normal", 6),
-  (8, "逃亡者は1週間以内にいずれかの空港より脱出すれば勝利となる", "アメリカ全土で1週間鬼ごっこしてみた。", "だいにグループ", "youtube", "https://example.com", "normal", 7);
+  (1, "バルス", "天空の城ラピュタ", "宮崎駿", "anime", "https://www.amazon.co.jp/%E5%A4%A9%E7%A9%BA%E3%81%AE%E5%9F%8E%E3%83%A9%E3%83%94%E3%83%A5%E3%82%BF-DVD-%E5%AE%AE%E5%B4%8E%E9%A7%BF/dp/B00K72NG0M", "normal", 1),
+  (2, "ブンブンハローYoutube", "【モンスト】鬼滅の刃コラボガチャ40連で確定からの超神引き！【ヒカキンゲームズ】", "HIKAKIN", "youtube", "https://youtu.be/w7olbu5dFsU", "normal", 1),
+  (3, "人間はなぜこうまでして自分の似姿をつくりたがるのかしらね", "攻殻機動隊", "草薙素子", "anime", "https://item.rakuten.co.jp/neowing-r/bvch-44004/", "normal", 2),
+  (4, "真実はいつも一つ！", "名探偵コナン", "青山剛昌", "anime", "https://tv.rakuten.co.jp/content/14627/", "normal", 1),
+  (5, "これはお前が始めた物語だろ", "進撃の巨人", "諫山創", "anime", "https://item.rakuten.co.jp/mangazenkan/si-506/", "normal", 3),
+  (6, "俺の大将は王様だが、お前のは玉だ！", "将棋", "東海オンエア", "youtube", "https://youtu.be/QscRqnBwCk0", "normal", 4),
+  (7, "グーみたいな奴がいて、チョキみたいな奴もいて、パーみたいな奴もいる。誰が一番強いか答えを知ってる奴はいるか？", "宇宙兄弟", "小山宙哉", "manga", "https://books.rakuten.co.jp/rk/97fe015f07974e419c268d30c62722de/", "normal", 6),
+  (8, "逃亡者は1週間以内にいずれかの空港より脱出すれば勝利となる", "アメリカ全土で1週間鬼ごっこしてみた。", "だいにぐるーぷ", "youtube", "https://youtu.be/rijrRFV_CWQ", "normal", 7);
+
 
 INSERT INTO
   comment(id, content, user_id, dialog_id)
